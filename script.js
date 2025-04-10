79382184ce130c70997b10a3ec4628615a4e033d
JillJ1 <jcjj.1104@gmail.com>
2025-04-10 18:09:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function checkPassword() {
     const errorMessage = document.getElementById("error-message");
 
     if (password === "1434Jill") {
-        window.location.href = "dashboard.html";
+        window.location.href ="dashboard.html";
     } else {
         errorMessage.textContent = "Oops! Try again, queen 👑";
     }
