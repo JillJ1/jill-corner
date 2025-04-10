a12979cf21f343ef90cefae459d1d0e9153f7e12
JillJ1 <jcjj.1104@gmail.com>
2025-04-10 17:45:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function checkPassword() {
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("error-message");
+
+    if (password === "1434Jill") {
+        window.location.href = "home.html";
+    } else {
+        errorMessage.textContent = "Oops! Try again, queen 👑";
+    }
+}
